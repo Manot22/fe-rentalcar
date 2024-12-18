47795495594dd8ed6739a7f8930e0b0b9e9da29a
Manot22 <arimanut41@gmail.com>
2024-12-18 23:07:13 +0700

    update car page

--- src/app/car/_components/CardCar.jsx
+++ src/app/car/_components/CardCar.jsx
@@ -43,52 +43,57 @@ export default function App() {
     }
   };
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 items-center justify-center gap-8">
-      {cars.map((item) => (
-        <Card className="" key={item.id}>
-          <CardHeader className="flex justify-between items-center">
-            <h1 className="text-md uppercase font-bold">{item.brand}</h1>
-            <div className="gap-4">
-              <Link href={`/car/edit/${item.id}`}>
-                <button>
-                  <FaEdit size={"20px"} />
+    <main>
+      <h3 className="capitalize text-medium font-serif py-2">
+        selmat datang {user.name}
+      </h3>
+      <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 items-center justify-center gap-8">
+        {cars.map((item) => (
+          <Card className="" key={item.id}>
+            <CardHeader className="flex justify-between items-center">
+              <h1 className="text-md uppercase font-bold">{item.brand}</h1>
+              <div className="gap-4">
+                <Link href={`/car/edit/${item.id}`}>
+                  <button>
+                    <FaEdit size={"20px"} />
+                  </button>
+                </Link>
+                <button onClick={() => handleDelete(item.id)}>
+                  <MdDelete className="text-red-500" size={"20px"} />
                 </button>
-              </Link>
-              <button onClick={() => handleDelete(item.id)}>
-                <MdDelete className="text-red-500" size={"20px"} />
-              </button>
-            </div>
-          </CardHeader>
-          <Divider />
-          <CardBody className="overflow-visible py-2">
-            <div className="pb-2 flex justify-end">
-              <Chip color="primary">{item.status}</Chip>
-            </div>
-            <Image
-              alt="NextUI Fruit Image with Zoom"
-              src={item.image}
-              width={250}
-              height={2}
-              className="pb-4"
-            />
+              </div>
+            </CardHeader>
             <Divider />
-            <h2 className="capitalize text-md font-bold py-2">{item.name}</h2>
-            <div className="grid grid-cols-2 justify-center items-center gap-4">
-              <p className="capitalize text-md font-semibold">{item.model}</p>
-              <h1 className="uppercase">{item.plateNumber}</h1>
-              <p className="text-sm">
-                {new Intl.NumberFormat("id-ID", {
-                  style: "currency",
-                  currency: "IDR",
-                }).format(item.dailyRate)}
-              </p>
-              <Button className="text-md hover:bg-gray-800 hover:text-white">
-                Sewa
-              </Button>
-            </div>
-          </CardBody>
-        </Card>
-      ))}
-    </div>
+            <CardBody className="overflow-visible py-2">
+              <div className="pb-2 flex justify-end">
+                <Chip color="primary">{item.status}</Chip>
+              </div>
+              <Image
+                alt="NextUI Fruit Image with Zoom"
+                src={item.image}
+                width={250}
+                height={2}
+                className="pb-4"
+              />
+              <Divider />
+              <h2 className="capitalize text-md font-bold py-2">{item.name}</h2>
+              <div className="grid grid-cols-2 justify-center items-center gap-4">
+                <p className="capitalize text-md font-semibold">{item.model}</p>
+                <h1 className="uppercase">{item.plateNumber}</h1>
+                <p className="text-sm">
+                  {new Intl.NumberFormat("id-ID", {
+                    style: "currency",
+                    currency: "IDR",
+                  }).format(item.dailyRate)}
+                </p>
+                <Button className="text-md hover:bg-gray-800 hover:text-white">
+                  Sewa
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+        ))}
+      </div>
+    </main>
   );
 }
